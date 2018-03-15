a5a85646961f9ffd7fae3c82006bb1569497231b
Joshua Ashton <joshua@nonagon.games>
2018-03-15 14:01:59 +0000

    VS 2015 + Compile Fixes

--- src/createvphysicsproject.bat
+++ src/createvphysicsproject.bat
@@ -1,2 +1,2 @@
-devtools\bin\vpc.exe +vphysics /define:sdk2013 /mksln vphysics_sdk2013.sln
-REM devtools\bin\vpc.exe +vphysics /define:asw /mksln vphysics_asw.sln
+devtools\bin\vpc.exe +vphysics /2013 /define:sdk2013 /mksln vphysics_sdk2013.sln
+REM devtools\bin\vpc.exe +vphysics /2013 /define:asw /mksln vphysics_asw.sln
