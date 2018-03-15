6e8fea3472a0a3ea251361c7dddb9f059fe7bd82
Joshua Ashton <joshua@nonagon.games>
2018-03-15 13:43:08 +0000

    Move to _sdk2013 and add beginning framework for ASW builds with _asw

--- src/createvphysicsproject.bat
+++ src/createvphysicsproject.bat
@@ -1,1 +1,2 @@
-devtools\bin\vpc.exe +vphysics /mksln vphysics.sln
+devtools\bin\vpc.exe +vphysics /define:sdk2013 /mksln vphysics_sdk2013.sln
+REM devtools\bin\vpc.exe +vphysics /define:asw /mksln vphysics_asw.sln
